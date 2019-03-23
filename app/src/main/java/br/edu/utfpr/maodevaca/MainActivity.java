0f59bf507c9148f344ca5f91a8bce5e4d3fc6bba
DanielUeda <ueda.yudi@gmail.com>
2019-03-23 16:00:30 -0300

    Ações botões inicio

--- app/src/main/java/br/edu/utfpr/maodevaca/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/maodevaca/MainActivity.java
@@ -2,12 +2,27 @@ package br.edu.utfpr.maodevaca;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView acProduto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        acProduto = findViewById(R.id.acProduto);
+    }
+
+    public void btIniciarOnClick(View view) {
+    }
+
+    public void btListarOnClick(View view) {
+    }
+
+    public void btAddProdutoOnClick(View view) {
     }
 }
