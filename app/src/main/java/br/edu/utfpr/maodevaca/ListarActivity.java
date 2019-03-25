cb8d76cf50b170b7d365b3e776f75412406553c4
Luiz Gustavo <oluizgustavob@gmail.com>
2019-03-25 19:48:33 -0300

    Implementar as lógicas de persisência e visualização dos produtos

--- app/src/main/java/br/edu/utfpr/maodevaca/ListarActivity.java
+++ app/src/main/java/br/edu/utfpr/maodevaca/ListarActivity.java
@@ -4,22 +4,53 @@ import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
+
+import br.edu.utfpr.maodevaca.dao.ProdutoDAO;
+import br.edu.utfpr.maodevaca.model.Produto;
 
 public class ListarActivity extends AppCompatActivity {
 
     private ListView lvProdutos;
+    private Button btMaisBarato;
+    private ArrayAdapter<Produto> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
-
-        lvProdutos = findViewById(R.id.lvProdutos);
+        recuperarViews();
     }
 
-    public void btCalcularOnClick(View view) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        popularListView();
+    }
+
+    private void popularListView() {
+        adapter = new ArrayAdapter<Produto>(this, android.R.layout.simple_list_item_1,
+                new ProdutoDAO(this).findAll());
+        this.lvProdutos.setAdapter(adapter);
+    }
+
+    private void recuperarViews() {
+        this.lvProdutos = findViewById(R.id.lvProdutos);
+        this.btMaisBarato = findViewById(R.id.btMaisBarato);
+        this.btMaisBarato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcularOMaisBarato();
+            }
+        });
+    }
+
+    private void calcularOMaisBarato() {
         //todo exibir o menor preço
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
--- app/src/main/java/br/edu/utfpr/maodevaca/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/maodevaca/MainActivity.java
@@ -6,39 +6,130 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
+
+import br.edu.utfpr.maodevaca.dao.DescricaoDAO;
+import br.edu.utfpr.maodevaca.dao.ProdutoDAO;
+import br.edu.utfpr.maodevaca.model.Descricao;
+import br.edu.utfpr.maodevaca.model.Produto;
 
 public class MainActivity extends AppCompatActivity {
 
     private AutoCompleteTextView acProduto;
+    private Button btAdicionar, btListar, btIniciar;
+    private EditText etQuantidade, etValor;
+    private ArrayAdapter<Descricao> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        acProduto = findViewById(R.id.acProduto);
-
-        /*ArrayAdapter meuAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,cidade);
-
-        acProduto.setAdapter(meuAdapter);*/
-        acProduto.setThreshold(2);
+        recuperarViews();
     }
 
-    public void btIniciarOnClick(View view) {
-        //todo delete
-
-        Toast.makeText(this,"Iniciando novo comparativo",Toast.LENGTH_LONG).show();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        atualizarAutoComplete();
     }
 
-    public void btListarOnClick(View view) {
+    private void recuperarViews(){
+        this.acProduto = findViewById(R.id.acProduto);
+
+        this.etQuantidade = findViewById(R.id.etQtd);
+        this.etValor = findViewById(R.id.etValor);
+
+        this.btAdicionar = findViewById(R.id.btAdicionar);
+        this.btListar = findViewById(R.id.btListar);
+        this.btIniciar = findViewById(R.id.btIniciar);
+
+        btAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adicionarProduto();
+            }
+        });
+
+        btListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listarProdutos();
+            }
+        });
+
+        btIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarBD();
+            }
+        });
+    }
+
+    private void iniciarBD() {
+        ProdutoDAO dao = new ProdutoDAO(this);
+        if (dao.deleteAll()) {
+            Toast.makeText(this, "Sucesso! Banco de dados pronto para inserções.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void listarProdutos() {
         Intent i = new Intent(this, ListarActivity.class);
         startActivity(i);
     }
 
-    public void btAddProdutoOnClick(View view) {
-        //todo add produto
+    private void adicionarProduto() {
+        if (!validarAntesDeSalvar()) {
+            return;
+        }
 
-        Toast.makeText(this,"Produto adicionado com sucesso",Toast.LENGTH_LONG).show();
+        Produto produto = new Produto();
+        produto.setDescricao(acProduto.getText().toString());
+        produto.setQuantidade(Double.parseDouble(etQuantidade.getText().toString()));
+        produto.setValor(Double.parseDouble(etValor.getText().toString()));
+        try {
+            ProdutoDAO dao = new ProdutoDAO(this);
+            if (dao.insert(produto)) {
+                Toast.makeText(this, "Produto salvo com sucesso!", Toast.LENGTH_SHORT).show();
+                atualizarAutoComplete();
+                zerarCampos();
+            } else {
+                Toast.makeText(this, "Não foi possível salvar o produto!", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean validarAntesDeSalvar() {
+        if (acProduto.getText().toString().isEmpty()) {
+            acProduto.setError("Informe a descrição do produto");
+            return false;
+        } else if (etQuantidade.getText().toString().isEmpty()) {
+            etQuantidade.setError("Informe a quantidade do produto");
+            return false;
+        } else if (etValor.getText().toString().isEmpty()){
+            etValor.setError("Informe o valor do produto");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void atualizarAutoComplete() {
+        adapter = new ArrayAdapter<Descricao>(this, android.R.layout.simple_list_item_1,
+                new DescricaoDAO(this).findAll());
+        this.acProduto.setAdapter(adapter);
+        acProduto.setThreshold(2);
+    }
+
+    private void zerarCampos() {
+        this.acProduto.setText("");
+        this.etQuantidade.setText("");
+        this.etValor.setText("");
+        this.acProduto.requestFocus();
     }
 }
